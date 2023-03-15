f92d2f30fef490cfc7efdbcfb68b49e14bbe8db6
Jeana Kim <jeana.kim@stonybrook.edu>
2023-03-15 14:43:43 +0900

    task 1 & 2 done!

--- func.py
+++ func.py
@@ -7,12 +7,23 @@ def add(x, y):
 #       return the square root of the given number. 
 
 def neg(x):
-    a = 2 * 3 # fill here
-    b = a + 13
+    return x * (-1)
     
+def sub(x, y):
+    return x - y
+
+def div(x, y):
+    return x / y
+
+def mult(x, y):
+    return x * y
+
+def exp(x, y):
+    return x ** y
 
 def sqrt(x):
-    ... # fill here
+    return x ** (1/2)# fill here
+
 
 # -------------------------------------- Task 2 -----------------------------------
 
@@ -23,8 +34,8 @@ a = 1
 b = -3
 c = 1
 
-x1 = ... # TODO: write a code to compute the first root of the quadratic equation
-x2 = ... # TODO: then do the same for the second root
+x1 = (-b+(b ** 2 - 4 * a * c)**(1/2))/(2 * a) # TODO: write a code to compute the first root of the quadratic equation
+x2 = (-b-(b ** 2 - 4 * a * c)**(1/2))/(2 * a) # TODO: then do the same for the second root
 # Note: Make sure to remove the ellipsis (...) when you're writing your code
 
 print("First root:" + str(x1))
