618d3025b8056af176a1beb691023cd3e8d1d4bd
Jeana Kim <jeana.kim@stonybrook.edu>
2023-03-15 20:20:50 +0900

    final touches!

--- func.py
+++ func.py
@@ -34,9 +34,20 @@ a = 1
 b = -3
 c = 1
 
+
+
 x1 = (-b+(b ** 2 - 4 * a * c)**(1/2))/(2 * a) # TODO: write a code to compute the first root of the quadratic equation
 x2 = (-b-(b ** 2 - 4 * a * c)**(1/2))/(2 * a) # TODO: then do the same for the second root
 # Note: Make sure to remove the ellipsis (...) when you're writing your code
 
+#I wasn't sure which one we were supposed to do so I did both.
+
+q1 = div(add(neg(b), (sqrt(sub(exp(b, 2), mult(mult(4, a), c))))), mult(2, a))
+q2 = div(sub(neg(b), (sqrt(sub(exp(b, 2), mult(mult(4, a), c))))), mult(2, a))
+
+
 print("First root: " + str(x1))
 print("Second root: " + str(x2))
+
+print("First root using functions: " + str(q1))
+print("Second root using functions: " + str(q2))
